b0b79259590cace14feca0762a3dd83376350dbb
zhilin <zhilinteh2@gmail.com>
2025-10-28 23:08:13 +0800

    profile ui bug

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -157,12 +157,11 @@ export default function Profile() {
                 </div>
               )}
             </div>
-          </div> 
-        )}       
-
-        <button className="edit-btn" onClick={() => navigate("/incomeSetup")}>
+                    <button className="edit-btn" onClick={() => navigate("/incomeSetup")}>
               <FaEdit /> Edit
         </button>
+          </div>
+        )}       
       </div>
 
       {/* Insurance Profile */}
