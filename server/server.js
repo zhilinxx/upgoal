3d46b0f5ca8c6b3518044ce613f45ab20867f146
zhilin <zhilinteh2@gmail.com>
2025-11-22 21:04:14 +0800

    .

--- server/server.js
+++ server/server.js
@@ -36,17 +36,14 @@ connectDB()
       process.env.CLIENT_URL // deployed frontend
     ]);
     app.use((req, res, next) => {
-      const origin = req.headers.origin;
-      if (!origin || ALLOWED.has(origin)) {
-        if (origin) res.setHeader("Access-Control-Allow-Origin", origin);
-        res.setHeader("Vary", "Origin");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-      }
+      res.setHeader("Access-Control-Allow-Origin", "*");  // only for testing
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
       if (req.method === "OPTIONS") return res.sendStatus(204);
       next();
     });
+
 
     app.use("/api/auth", authRoutes);
     app.use("/api/profile", profileRoutes);
