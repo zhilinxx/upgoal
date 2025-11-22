74099224fb4eec86fabeea38671f48bafc22b090
zhilin <zhilinteh2@gmail.com>
2025-11-22 22:31:05 +0800

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,7 @@ const PAGE_TITLES = {
 
 import axios from "axios";
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true, // this ensures cookies are sent
 });
 
--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true, // ✅ important to send cookies
 });
 
