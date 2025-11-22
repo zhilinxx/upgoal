7c8b7404af2b0a4f2bc0bd2d83a25af35c6a46c4
zhilin <zhilinteh2@gmail.com>
2025-11-22 19:23:30 +0800

    .

--- server/config/db.js
+++ server/config/db.js
@@ -7,9 +7,9 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST ?? 'localhost',
   user: process.env.DB_USER ?? 'root',
-  password: process.env.DB_PASSWORD ?? '',
-  database: process.env.DB_DATABASE ?? 'upgoals',
-  port: Number(process.env.DB_PORT ?? 3306),
+  password: process.env.DB_PASS ?? '',
+  database: process.env.DB_NAME ?? 'upgoals',
+  port: Number(process.env.PORT ?? 3306),
   ssl: {
     rejectUnauthorized: false
   },
