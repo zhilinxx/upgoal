0b29018038ebd4768ac6d60bb92b54bacaadc7e2
zhilin <zhilinteh2@gmail.com>
2025-11-23 13:48:31 +0800

    .

--- client/src/api/profileAPI.js
+++ client/src/api/profileAPI.js
@@ -4,5 +4,5 @@ import { API } from "./auth"; // reuse your configured axios instance
 
 // === Profile functions ===
 export const getProfile = async () => {
-  return API.get("/profile/me", { withCredentials: true });
+  return API.get("/profile/me");
 };
