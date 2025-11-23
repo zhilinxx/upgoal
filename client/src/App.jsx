2fac7f7fbfaa5ac6a8aa60560b36960bbe85ca77
zhilin <zhilinteh2@gmail.com>
2025-11-23 13:27:12 +0800

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,7 @@ const PAGE_TITLES = {
 
 import axios from "axios";
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${process.env.VITE_API_URL}/api`,
   withCredentials: true, // this ensures cookies are sent
 });
 
--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${process.env.VITE_API_URL}/api`,
   withCredentials: true, // ✅ important to send cookies
 });
 
