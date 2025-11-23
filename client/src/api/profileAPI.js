7e0e9fc615dc2145aa9b7e45541e30b893d0950e
zhilin <zhilinteh2@gmail.com>
2025-11-23 23:06:12 +0800

    fix profile bug

--- client/src/api/profileAPI.js
+++ client/src/api/profileAPI.js
@@ -4,5 +4,5 @@ import { API } from "./auth"; // reuse your configured axios instance
 
 // === Profile functions ===
 export const getProfile = async () => {
-  return API.get("/profile/me");
+  API.get("/profile/me", { withCredentials: true });
 };
