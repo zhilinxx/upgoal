63a5878d839ad46feb6f8f4a7cc26c4d6383159a
zhilin <zhilinteh2@gmail.com>
2025-11-25 15:09:52 +0800

    /

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
-      from: "UpGoal <onboarding@resend.dev>",
+      from: "UpGoal <upgoals@upgoalsupport.xyz>",
       to,
       subject,
       html,
